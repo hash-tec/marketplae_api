71d4c3a85c8d2f479d78a9f78272eb7f4efa169d
hashry <hashtech03@gmail.com>
2025-03-11 00:27:58 +0100

    Added a catgory field to the Product model

--- products/models.py
+++ products/models.py
@@ -5,12 +5,16 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Product(models.Model):
+    category_choice =(('men', "Men's Wear"), ('women', "Women's Wear"),
+                      ('footwears', "Footwears"), ('bags', "Bags"))
+    
     seller = models.ForeignKey( Customer, on_delete=models.CASCADE, verbose_name=_("Seller"))
     product_name = models.CharField(_("Product Name"), max_length=200)
     brand = models.CharField(_("Brand"), max_length=50, blank=False)
     description = models.TextField(_("Description"), blank=False)
     price = models.DecimalField(_("Price"), max_digits=10, decimal_places=2, blank=False )
     discount_percentage= models.DecimalField(_("Discount Percentage"), max_digits=10, decimal_places=0, null=True, blank=True)
+    category =  models.CharField(_("Category"), choices=category_choice, max_length=10, null = True )
     image = models.FileField(_("Image"), upload_to="items_images", null=True)
     slug = models.SlugField(default="", null=True)
     date_created = models.DateField(auto_now_add=True)
