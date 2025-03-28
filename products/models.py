3a2a8e7fc7cacbf6dfff13bd59e127834985f1d3
hashry <hashtech03@gmail.com>
2025-03-28 21:48:01 +0100

    Added more choices to the category

--- products/models.py
+++ products/models.py
@@ -6,16 +6,24 @@ from django.utils.text import slugify
 
 # Create your models here.
 class Product(models.Model):
-    category_choice =(('men', "Men's Wear"), ('women', "Women's Wear"),
-                      ('footwears', "Footwears"), ('bags', "Bags"))
-    
+    category_choice =[
+        ("Man Fashion",(
+         ("Man Shirt", "Man T-Shirt"),("Man Shoes", "Man Shoes"),
+         ("Man Work Equipment", "Man Work Equipment"), ("Man Pant", "Man Pants"),
+         ("Man Underwear", "Man Underwear"))),
+
+         ("Women's fashion",(
+          ("Dress", "Dress"), ("Woman T-Shirt", "Woman T-shirt"), 
+          ("Woman Pants", "Woman Pants"), ("Skirts", "skirts"), ("Bags", "Bags"),
+          ("High Heels", "High Heels"), ("Bikini", "Bikini")))
+    ]
     seller = models.ForeignKey( Customer, on_delete=models.CASCADE, verbose_name=_("Seller"))
     product_name = models.CharField(_("Product Name"), max_length=200)
     brand = models.CharField(_("Brand"), max_length=50, blank=False)
     description = models.TextField(_("Description"), blank=False)
     price = models.DecimalField(_("Price"), max_digits=10, decimal_places=2, blank=False )
     discount_percentage= models.DecimalField(_("Discount Percentage"), max_digits=10, decimal_places=0, null=True, blank=True)
-    category =  models.CharField(_("Category"), choices=category_choice, max_length=10, null = True )
+    category =  models.CharField(_("Category"), choices=category_choice, max_length=50, null = True )
     image = models.FileField(_("Image"), upload_to="items_images", null=True)
     slug = models.SlugField(default="", null=True)
     date_created = models.DateField(auto_now_add=True)
