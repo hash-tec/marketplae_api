6042ba735e38fa6eb1bb1ecdbf62ddb9d3775d98
hashry <hashtech03@gmail.com>
2025-03-30 23:41:27 +0100

    Moment before i wipe databse and cleear the migrations

--- coupons/models.py
+++ coupons/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from access.models import Customer
+from django.conf import settings
 
 # Create your models here.
 class Coupon(models.Model):
+    user = models.ForeignKey(Customer, on_delete=models.CASCADE)
     coupon_name = models.CharField(max_length = 150)
+    code = models.CharField(max_length=7, unique=True)
+    identifier =  models.CharField(max_length=50)
     percentage_off = models.IntegerField()
     date_created = models.DateTimeField(auto_now_add=True)
